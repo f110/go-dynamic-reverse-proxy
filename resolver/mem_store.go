dacf6a3fcc858ca8e5b043311824e1ba254ad1a0
Fumihiro Itoh <fmhrit@gmail.com>
2014-04-19 00:47:44 +0900

    add on memory storage

---
+++ resolver/mem_store.go
@@ -0,0 +1,27 @@
+package resolver
+
+type MemStorage struct {
+	Table    map[string]string
+}
+
+func NewMemStorage() *MemStorage {
+    return &MemStorage{Table: make(map[string]string)}
+}
+
+func (this *MemStorage) Get(key string) string {
+	return this.Table[key]
+}
+
+func (this *MemStorage) Set(key string, value string) {
+	this.Table[key] = value
+}
+
+func (this *MemStorage) List() []string {
+	var keys []string
+
+	for k := range this.Table {
+		keys = append(keys, k)
+	}
+
+	return keys
+}
